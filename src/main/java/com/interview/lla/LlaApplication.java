8292a3333459be289897b6d11117f236641e4001
Shayan Ahmad <getshayanhere@gmail.com>
2023-03-30 13:19:18 +0100

    first commit

---
+++ src/main/java/com/interview/lla/LlaApplication.java
@@ -0,0 +1,13 @@
+package com.interview.lla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/lla/LlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.lla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
